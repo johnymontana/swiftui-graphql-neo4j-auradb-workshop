9f605414cc7b4f1ee2e755e3157f940d6dfb7d3d
johnymontana <lyonwj@gmail.com>
2022-06-07 04:36:42 -0500

    add graphql

---
+++ graphql/README.md
@@ -0,0 +1,20 @@
+[![Open in CodeSandbox](https://img.shields.io/badge/Open%20in-CodeSandbox-blue?style=flat-square&logo=codesandbox)](https://codesandbox.io/s/github/johnymontana/book-graph/tree/main/graphql/auth?file=/schema.graphql)
+
+
+
+# News Graph GraphQL API Using Apollo Server
+
+Launch in [Codesandbox](https://codesandbox.io/s/github/johnymontana/swiftui-graphql-neo4j-auradb-workshop/tree/main/graphql?file=/schema.graphql)
+
+This directory contains code for a GraphQL API using the [Neo4j GraphQL library.](https://neo4j.com/product/graphql-library/)
+
+## Quickstart
+
+1. Update `.env` with your Neo4j credentials
+2. `npm i`
+3. `npm start`
+4. Open `http://localhost:4000` in your web browser to open GraphQL Playground
+
+## GraphQL Schema
+
+See [schema.graphql](schema.graphql) for the GraphQL type definitions.
---
+++ graphql/index.js
@@ -0,0 +1,43 @@
+const { Neo4jGraphQL } = require("@neo4j/graphql");
+const { ApolloServer } = require("apollo-server");
+const {
+  ApolloServerPluginLandingPageGraphQLPlayground,
+} = require("apollo-server-core");
+const neo4j = require("neo4j-driver");
+const fs = require("fs");
+const dotenv = require("dotenv");
+const path = require("path");
+
+// Load contents of .env as environment variables
+dotenv.config();
+
+// Create Neo4j driver instance
+const driver = neo4j.driver(
+  process.env.NEO4J_URI,
+  neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD)
+);
+
+// Load GraphQL type definitions from schema.graphql file
+const typeDefs = fs
+  .readFileSync(path.join(__dirname, "schema.graphql"))
+  .toString("utf-8");
+
+// Create executable GraphQL schema from GraphQL type definitions,
+// using @neo4j/graphql to autogenerate resolvers
+const neoSchema = new Neo4jGraphQL({
+  typeDefs,
+  driver,
+});
+
+// Create a new Apollo Server instance using our Neo4j GraphQL schema
+neoSchema.getSchema().then((schema) => {
+  const server = new ApolloServer({
+    schema,
+    playground: true,
+    introspection: true,
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
+  });
+  server.listen().then(({ url }) => {
+    console.log(`GraphQL server ready at ${url}`);
+  });
+});
